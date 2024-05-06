d910ff80c3888cfcbb6595272aeae0d114eb3bdd
Julio Lopez <58756482+donlopez@users.noreply.github.com>
2024-05-05 21:40:24 -0400

    modified:   src/App.js new file:   src/components/ProjectItem.js modified:   src/helpers/ProjectList.txt modified:   src/pages/Projects.js renamed:    src/styles/ProjectDisplay.css -> src/styles/ProjectDisplay.txt

--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -1,19 +1,17 @@
 import React from "react";
 import ProjectItem from "../components/ProjectItem";
-import { ProjectList } from "../helpers/ProjectList";
+import server from "../assets/server.jpeg";
+import python from "../assets/python.jpeg";
 
 import "../styles/Projects.css";
 
 function Projects() {
   return (
     <div className="projects">
-      <h1> My Personal Projects</h1>
+      <h1> My Personal Projects </h1>
       <div className="projectList">
-        {ProjectList.map((project, idx) => {
-          return (
-            <ProjectItem id={idx} name={project.name} image={project.image} />
-          );
-        })}
+        <ProjectItem name="Cloud Solutions" image={server}/>
+        <ProjectItem name="Data Analisys" image={python}/>
       </div>
     </div>
   );
